9c53d25de5665701e578778d07ed788c353b3aec
Sebastien Coppolani <sebcoppo@yahoo.fr>
2020-08-07 18:46:32 +0200

    Attached Photo

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,7 +9,7 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
-    redirect_to cocktail_path(@cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
       render 'cocktails/show'
     end
